d31c7cf4cb92f8c1a691a9a486ffaa31a2201e72
Proliecan <65495509+Proliecan@users.noreply.github.com>
2023-05-23 19:42:32 +0200

    Simple http server

---
+++ index.js
@@ -0,0 +1,16 @@
+var host = "127.0.0.1";
+var port = 1337;
+var express = require("express");
+var path = require("path");
+var dirname = path.join(__dirname, "public");
+
+var app = express();
+app.use('/', express.static(dirname));
+app.listen(port, host);
+
+// blue color for console.log
+const blue = '\x1b[34m';
+const reset = '\x1b[0m';
+console.log("Hosting directory: ", dirname);
+console.log("Server running at", blue, "http://" + host + ":" + port, reset);
+console.log("Press Ctrl+C to stop server");
